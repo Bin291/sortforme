102e3f9822fc4c2930e7e196ec62613570cb69b9
Bin291 <phuongbinh200429@gmail.com>
2025-05-17 11:23:50 +0700

    feat: update pack

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "sortforme-9e3a4", appId: "1:1026570661908:web:6fe9780564748671707701", storageBucket: "sortforme-9e3a4.firebasestorage.app", apiKey: "AIzaSyC2d128-sM_jQlRqe1GKIjCMOLjX_M2YSU", authDomain: "sortforme-9e3a4.firebaseapp.com", messagingSenderId: "1026570661908", measurementId: "G-SFWSEGFH9H" })), provideAuth(() => getAuth())]
 };
